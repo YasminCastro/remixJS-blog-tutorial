3f854060be3a0f1e752dd28c4eab25c6449c583f
Yasmin <yasminsdcastro@gmail.com>
2022-05-05 11:05:35 -0300

    first commit

---
+++ app/routes/posts/index.tsx
@@ -0,0 +1,38 @@
+import { json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+import { getPosts } from "~/models/post.server";
+
+interface IPost {
+  slug: string;
+  title: string;
+}
+
+type LoaderData = { posts: IPost[] };
+
+export const loader = async () => {
+  return json<LoaderData>({
+    posts: await getPosts(),
+  });
+};
+
+export default function Posts() {
+  const { posts } = useLoaderData() as LoaderData;
+  console.log(posts);
+  return (
+    <main>
+      <h1>Posts</h1>
+      <Link to="admin" className="text-red-600 underline">
+        Admin
+      </Link>
+      <ul>
+        {posts.map((post: IPost) => (
+          <li key={post.slug}>
+            <Link to={post.slug} className="text-blue-600 underline">
+              {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
